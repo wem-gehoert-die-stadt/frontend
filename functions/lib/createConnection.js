3f099cd59db96a7bb41540f6aee7b3cdb27f28d1
tlmn <mail@tlmn.io>
2022-07-27 15:23:20 +0200

    remove console.log

--- functions/lib/createConnection.js
+++ functions/lib/createConnection.js
@@ -13,8 +13,6 @@ const connection = {
 };
 
 export const db = pgp(connection);
-console.log("host: ", env.PG_HOST,", database: ", env.PG_DATABASE, ", user: ", env.PG_USER, ", password: ", env.PG_PASSWORD);
-console.log("database", db);
 
 export const { PreparedStatement: PS } = pgp;
 export const { GLOBAL_DAILY_LIMIT } = env;
